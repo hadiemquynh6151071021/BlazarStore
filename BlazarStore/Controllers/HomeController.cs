06d2754db2d5648ef73c3007c7e7ab76a0a1d769
DiemQuynh <haquynhla2002@gmail.com>
2023-10-11 20:11:09 +0700

    Add project files.

---
+++ BlazarStore/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlazarStore.Models;
+
+namespace BlazarStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BlazarShopEntities db = new BlazarShopEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RenderCategory()
+        {
+            List<CategoryByGender> categoryByGenders = db.CategoryByGenders.ToList();
+            return PartialView("_listCategory", categoryByGenders);
+        }
+
+        public ActionResult RenderProductInHome()
+        {
+            List<Product> products = db.Products.Take(10).ToList();
+            return PartialView("_listProductInHome", products);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BlazarStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlazarStore.Startup))]
+namespace BlazarStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
